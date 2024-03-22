5cfd77dafb95fa545e60f1d5297b39b53a11dcd5
Hossein Mansouri <hsnmsri82@gmail.com>
2024-03-22 22:27:46 +0330

    complete local proxy server

--- window.py
+++ window.py
@@ -1,7 +1,12 @@
 import customtkinter
+import socket
+import colors
+import random
+import threading
 
 
 class Window(customtkinter.CTk):
+
     def __init__(self):
         super().__init__()
         # self._set_appearance_mode("system")
@@ -12,6 +17,7 @@ class Window(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=2)
         self.grid_rowconfigure(1, weight=8)
         self.grid_rowconfigure(2, weight=1)
+        self.server_running = False
         # frames ---
         self.frameMain = customtkinter.CTkFrame(self, fg_color="transparent")
         self.frameMain.grid(row=0, column=0, sticky="w")
@@ -27,9 +33,7 @@ class Window(customtkinter.CTk):
         self.frameLog.grid_rowconfigure(0, weight=1)
         self.frameLog.grid_columnconfigure(0, weight=1)
 
-        self.frameStatus = customtkinter.CTkFrame(
-            self, fg_color="transparent"
-        )
+        self.frameStatus = customtkinter.CTkFrame(self, fg_color="transparent")
         self.frameStatus.grid(row=2, column=0, sticky="w")
         self.frameStatus.grid_rowconfigure(0, weight=1)
         self.frameStatus.grid_columnconfigure(0, weight=1)
@@ -54,7 +58,7 @@ class Window(customtkinter.CTk):
         self.txtServerPort.grid(row=0, column=3)
 
         self.btnToggleServerStatus = customtkinter.CTkButton(
-            self.frameMain, text="Start", width=70
+            self.frameMain, text="Start", width=70, command=self.main
         )
         self.btnToggleServerStatus.grid(row=0, column=4, padx=20)
 
@@ -68,3 +72,76 @@ class Window(customtkinter.CTk):
             self.frameStatus, text="not running", text_color="red"
         )
         self.lblStatus.grid(row=0, column=1, padx=10)
+
+    def main(self):
+        if not self.server_running:
+            proxy_address = (
+                self.txtServerIp.get() if self.txtServerIp.get() != "" else "127.0.0.1"
+            )
+            proxy_port = int(
+                self.txtServerPort.get() if self.txtServerPort.get() != "" else "8888"
+            )
+            proxy_client = 5
+
+            # set available port
+            while True:
+                try:
+                    proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    proxy_socket.bind((proxy_address, proxy_port))
+                    proxy_socket.listen(proxy_client)
+                except Exception as error:
+                    if error.errno == 98:
+                        self.txtLog.insert(
+                            "end", f"Server already in use on port {proxy_port}\n"
+                        )
+                        proxy_port = random.randint(1111, 9999)
+                        self.txtLog.insert("end", f"Try again on port {proxy_port} \n")
+                        continue
+                break
+
+            # server running style
+            self.btnToggleServerStatus.configure(text="Stop")
+            self.lblUrl.configure(text=f"{proxy_address}:{proxy_port}")
+            self.lblStatus.configure(text="running", text_color="green")
+            self.txtServerIp.configure(state="disable")
+            self.txtServerPort.configure(state="disable")
+
+            self.txtLog.insert(
+                "end", "Lestening on : " + f"{proxy_address}:{proxy_port}\n"
+            )
+            self.server_running = True
+            server_thread = threading.Thread(
+                target=self.start_server, args=(proxy_socket,)
+            )
+            server_thread.start()
+
+        else:
+            self.server_running = False
+            self.txtLog.insert("end", "\nServer Stopped!\n")
+            # server running style
+            self.btnToggleServerStatus.configure(text="Start")
+            self.lblStatus.configure(text="not running", text_color="red")
+            self.txtServerIp.configure(state="normal")
+            self.txtServerPort.configure(state="normal")
+
+    def handle_client(self, client_socket):
+        try:
+            request = client_socket.recv(4096)
+            self.txtLog.insert("end", "Client sended data : \n")
+            self.txtLog.insert("end", request.decode())
+            client_socket.close()
+        except Exception as error:
+            print("error 127")
+            exit()
+
+    def start_server(self, proxy_socket):
+        while self.server_running:
+            self.txtLog.insert("end", "\nConnection Accepted : \n")
+
+            try:
+                client_socket, address = proxy_socket.accept()
+                self.txtLog.insert("end", f"Address : {address[0]}:{address[1]}\n")
+                self.handle_client(client_socket)
+            except Exception as error:
+                print("error 117")
+                exit()
